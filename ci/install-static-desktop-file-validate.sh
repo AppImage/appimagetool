c2fe3826d58225eb6b6942e603535aef0b5c422d
TheAssassin <theassassin@assassinate-you.net>
2024-12-08 18:01:31 +0100

    Build latest desktop-file-utils statically with meson

--- ci/install-static-desktop-file-validate.sh
+++ ci/install-static-desktop-file-validate.sh
@@ -25,11 +25,9 @@ pushd "$build_dir"
 wget -c "https://gitlab.freedesktop.org/xdg/desktop-file-utils/-/archive/"$version"/desktop-file-utils-"$version".tar.gz"
 tar xf desktop-file-utils-*.tar.gz
 cd desktop-file-utils-*/
-# The next 2 lines are a workaround for: checking build system type... ./config.guess: unable to guess system type
-wget 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' -O config.guess
-wget 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD' -O config.sub
-autoreconf --install # https://github.com/shendurelab/LACHESIS/issues/31#issuecomment-283963819
-./configure CFLAGS=-no-pie LDFLAGS=-static
+
+# setting LDFLAGS as suggested in https://mesonbuild.com/Creating-Linux-binaries.html#building-and-installing
+env LDFLAGS=-static meson setup build --prefer-static --default-library=static
 
 if [[ "${GITHUB_ACTIONS:-}" != "" ]]; then
     jobs="$(nproc)"
@@ -37,11 +35,5 @@ else
     jobs="$(nproc --ignore=1)"
 fi
 
-make -j"$jobs"
-
-# TODO: Find more elegant way to do this
-cd src/
-gcc -static -o desktop-file-validate keyfileutils.o validate.o validator.o mimeutils.o -lglib-2.0 -lintl
-strip desktop-file-validate
-cp desktop-file-validate /usr/bin/
-cd ..
+ninja -C build -j "$jobs" -v
+ninja -C build -j 1 -v install
