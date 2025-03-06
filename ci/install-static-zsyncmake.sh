7f11ca3bc5c7220b07d4637be6cbb7a85314be48
TheAssassin <theassassin@assassinate-you.net>
2025-03-06 02:07:03 +0100

    Revert "Use Debian's zsync patches"
    This reverts commit b340e541594f4ec219d1f838723aedc61ac265dd.

--- ci/install-static-zsyncmake.sh
+++ ci/install-static-zsyncmake.sh
@@ -29,20 +29,6 @@ tar xf zsync_"$version"*.tar.bz2
 
 cd zsync-*/
 
-# use Debian's patches, too
-wget \
-    https://sources.debian.org/data/main/z/zsync/0.6.2-6/debian/patches/buildsystem.diff \
-    https://sources.debian.org/data/main/z/zsync/0.6.2-6/debian/patches/manpages.diff \
-    https://sources.debian.org/data/main/z/zsync/0.6.2-6/debian/patches/clienthttp.diff \
-    https://sources.debian.org/data/main/z/zsync/0.6.2-6/debian/patches/fix-build-with-gcc-14.patch
-
-sha256sum -c <<\EOF
-f14a7ffcb3bdadba252c9afbe5e8da358fc77f20072ae5b21475bdfc44b50975  buildsystem.diff
-085e9b5856bb8ac0d8567e977a9d5fafc6c19df02b7cfb8e011fedaf2862eaad  clienthttp.diff
-f04bba2866c47b8597723d49eec3f1fd50e40a08171e261bf0fc262852ba17e5  manpages.diff
-35cbce69194743df1f3f4b9bc9ad36a4c440b117c6a17c268023c642c0d50650  fix-build-with-gcc-14.patch
-EOF
-
 # custom patch to at least ignore flags passed after the first parameter
 # see https://github.com/AppImage/appimagetool/pull/93 for more information
 cat > argparser.patch <<\EOF
@@ -76,7 +62,7 @@ index 191b527..d86f130 100644
              if (!instream) {
 EOF
 
-for i in *.diff *.patch; do
+for i in *.patch; do
     echo "Applying patch $i..."
     patch -p1 < "$i"
 done
