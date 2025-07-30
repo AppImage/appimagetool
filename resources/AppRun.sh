7da00975797449e4fd3d5049c62d47bc956e1cc2
mxmlnkn <mxmlnkn@github.de>
2025-07-30 10:10:05 +0200

    First call readlink on argument 0 and then dirname
    Fixes support for being executed via a symbolic link.

--- resources/AppRun.sh
+++ resources/AppRun.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-this_dir="$(readlink -f "$(dirname "$0")")"
+this_dir="$(dirname -- "$(readlink -f -- "$0")")"
 
 # make appimagetool prefer the bundled mksquashfs
 export PATH="$this_dir"/usr/bin:"$PATH"
